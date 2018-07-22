ce26ab0bd7418782f72cd39acc577fc60dcd0cc9
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2018-07-22 20:11:21 +0530

    Add files via upload
    Second commit

---
+++ nine.java
@@ -0,0 +1,21 @@
+
+public class nine
+{
+public static void main(String[] args)
+ {
+int a,b,c;
+ s1:for( a=1;a<1000;a++)
+    {
+        for( b=1;b<1000;b++)
+        {
+            c=1000-a-b;
+            if(c>0 && (Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2)))
+            {
+              System.out.println(a*b*c);
+              break s1;
+            }
+        }
+    }
+    
+  }
+}
